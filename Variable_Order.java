1f3e2b31521c6ff9b5800f9847a1b39c59fe0790
pascualbry <63685506+pascualbry@users.noreply.github.com>
2020-04-15 22:32:01 +0800

    Create Variable_Order.java

---
+++ Variable_Order.java
@@ -0,0 +1,147 @@
+package Database;
+
+public class Variable_Order {
+
+	private String tag;
+	private String dateReceive;
+	private String clientName;
+	private String newprice;
+	private String contactNumber;
+	private float total;
+	private float finalTotal;
+	private String location;
+	private int counter;
+	private String product;
+	private int quantity;
+	private String category;
+	private String incharge;
+	private String accountNumber;
+	private String name;
+	private String email;
+	private String username;
+	private String password;
+	private String userType;
+	
+
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getIncharge() {
+		return incharge;
+	}
+	public void setIncharge(String incharge) {
+		this.incharge = incharge;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getSerialNumber() {
+		return product;
+	}
+	public void setSerialNumber(String product) {
+		this.product = product;
+	}
+	public int getCounter() {
+		return counter;
+	}
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getNewprice() {
+		return newprice;
+	}
+	public void setNewprice(String newprice) {
+		this.newprice = newprice;
+	}	
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public String getDateReceive() {
+		return dateReceive;
+	}
+	public void setDateReceive(String dateReceive) {
+		this.dateReceive = dateReceive;
+	}
+	public String getClientName() {
+		return clientName;
+	}
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public float getTotal() {
+		return total;
+	}
+	public void setTotal(float total) {
+		this.total = total;
+	}
+	public float getFinalTotal() {
+		return finalTotal;
+	}
+	public void setFinalTotal(float finalTotal) {
+		this.finalTotal = finalTotal;
+	}
+	
+}
