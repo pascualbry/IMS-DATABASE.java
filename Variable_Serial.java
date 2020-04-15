4d01b2335a020f85680a04fed6e587353f4404a7
pascualbry <63685506+pascualbry@users.noreply.github.com>
2020-04-15 22:30:42 +0800

    Create Variable_Serial.java

---
+++ Variable_Serial.java
@@ -0,0 +1,21 @@
+package Database;
+
+public class Variable_Serial {
+
+	private String serialNumber;
+	private String name;
+	
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
